633b69b5e67694c351e5bc50b07fb7fcb770c57b
A01378360 <65176620+A01378360@users.noreply.github.com>
2020-06-16 10:57:58 -0500

    Add files via upload

---
+++ felpy.py
@@ -0,0 +1,5 @@
+[print('Happy Birthday %s'%('dear Python' if x==2 else 'to you')) for x in range(4)]
+for k in range(4):print"Happy birthday",k==2 and"Dear Python"or"to You"
+1
+for k in range(4):print"Happy birthday",k==2 and"Dear Python"or"to You"
+ 
